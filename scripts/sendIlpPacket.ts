4e79bcad57d49182d4f3ee1131f4998108ddf99a
DonChangfoot <donovanchangfoot@gmail.com>
2019-09-10 16:01:26 +0200

    testing actions

--- scripts/sendIlpPacket.ts
+++ scripts/sendIlpPacket.ts
@@ -2,7 +2,6 @@ import axios from 'axios'
 import { deserializeIlpReply, IlpPrepare, serializeIlpPrepare } from 'ilp-packet'
 import { STATIC_CONDITION } from '../packages/rafiki-core/src'
 
-
 async function run () {
   const prepare: IlpPrepare = {
     amount: '1',
